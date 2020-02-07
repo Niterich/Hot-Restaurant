2cbe0601dea49b6f5fa11c560cbd4cf4a7648bcb
blinkjuan82 <ceratio24@gmail.com>
2020-02-06 21:07:38 -0700

    server.js works and pages interract with each other

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const path = ("path");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,6 +11,12 @@ app.use(express.json());
 // Routes
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"))
+});
+app.get("/reserve", (req, res) => {
+    res.sendFile(path.join(__dirname, "reserve.html"))
+});
+app.get("/tables", (req, res) => {
+    res.sendFile(path.join(__dirname, "tables.html"))
 });
 
 app.listen(PORT, function() {
---
+++ wait.js
@@ -0,0 +1,9 @@
+class WaitingPerson {
+    constructor(name, email, phone, id){
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.id = id;
+    }
+}
+module.exports = wait
